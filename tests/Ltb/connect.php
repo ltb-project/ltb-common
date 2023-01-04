a58499a88ddf8f6d54ffe08f74eb5cb83d6809b4
philha <philha@worteks.com>
2023-01-04 08:03:34 +0100

    namespae is Ltb Ltb\Ldap is a class
    - namespace is case sensitive and is only Ltb, Ldap is class name.
    - test it
      php tests/Ltb/connect.php
    should return
    PHP Warning:  ldap_bind(): Unable to bind to server: Can't contact LDAP server in ...
    LDAP - Bind error -1  (Can't contact LDAP server)

--- tests/Ltb/connect.php
+++ tests/Ltb/connect.php
@@ -2,9 +2,9 @@
 
 namespace Ltb;
 
-# require __DIR__ . '/../../vendor/autoload.php';
+require __DIR__ . '/../../vendor/autoload.php';
 
-
-require __DIR__ . '/../../src/Ltb/Ldap.php';
+# if autoload fails :
+# require __DIR__ . '/../../src/Ltb/Ldap.php';
 
 Ldap::connect('ldap://127.0.0.1',false,'cn=admin','binpwd',10);
