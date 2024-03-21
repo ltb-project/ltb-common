8d08c8d43c3ca5a50b6efea9afe05630fc74e5b3
Markus Beckschulte <markusbeckschulte96@outlook.de>
2024-03-21 15:37:09 +0100

    Mail: don't use global vars (#4)

--- src/Ltb/Mail.php
+++ src/Ltb/Mail.php
@@ -1,51 +1,70 @@
 <?php namespace Ltb;
 
 use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-use PHPMailer\PHPMailer\SMTP;
 
+/**
+ * How the class should be used:
+ *  - initialize the mailer with init_mailer_locally
+ *  - use send_mail_global to send mails
+ * 
+ * For compatibility reasons, you can also use init_mailer_legacy (or init_mailer with 0 arguments) that will return the mailer.
+ * The returned mailer can than be used to call send_mail directly, but this should also not be used!
+ */
 final class Mail {
+    
+    private static $mailer = null;
 
     # Mail functions
-    # rely on global mail configured with $mail_XXXX variables
-
-    # init $mailer from config $mail_XXXX variables
-    # legacy code to be compliant with self-service-password existing configuration
-    static function init_mailer()
+    
+    # initializes the mailer
+    static function init_mailer_locally(
+            $mail_priority,
+            $mail_charset,
+            $mail_contenttype,
+            $mail_wordwrap,
+            $mail_sendmailpath,
+            $mail_protocol,
+            $mail_smtp_debug,
+            $mail_debug_format,
+            $mail_smtp_host,
+            $mail_smtp_port,
+            $mail_smtp_secure,
+            $mail_smtp_autotls,
+            $mail_smtp_auth,
+            $mail_smtp_user,
+            $mail_smtp_pass,
+            $mail_smtp_keepalive,
+            $mail_smtp_options,
+            $mail_smtp_timeout
+    )
     {
 
         #==============================================================================
         # Email Config
         #==============================================================================
-
-        global $mailer;
-        global $mail_priority,  $mail_charset, $mail_contenttype,  $mail_wordwrap, $mail_sendmailpath;
-        global $mail_protocol,  $mail_smtp_debug, $mail_debug_format, $mail_smtp_host, $mail_smtp_port;
-        global $mail_smtp_secure, $mail_smtp_autotls, $mail_smtp_auth, $mail_smtp_user, $mail_smtp_pass;
-        global $mail_smtp_keepalive, $mail_smtp_options, $mail_smtp_timeout;
-
-        $mailer= new PHPMailer;
-
-        $mailer->Priority      = $mail_priority;
-        $mailer->CharSet       = $mail_charset;
-        $mailer->ContentType   = $mail_contenttype;
-        $mailer->WordWrap      = $mail_wordwrap;
-        $mailer->Sendmail      = $mail_sendmailpath;
-        $mailer->Mailer        = $mail_protocol;
-        $mailer->SMTPDebug     = $mail_smtp_debug;
-        $mailer->Debugoutput   = $mail_debug_format;
-        $mailer->Host          = $mail_smtp_host;
-        $mailer->Port          = $mail_smtp_port;
-        $mailer->SMTPSecure    = $mail_smtp_secure;
-        $mailer->SMTPAutoTLS   = $mail_smtp_autotls;
-        $mailer->SMTPAuth      = $mail_smtp_auth;
-        $mailer->Username      = $mail_smtp_user;
-        $mailer->Password      = $mail_smtp_pass;
-        $mailer->SMTPKeepAlive = $mail_smtp_keepalive;
-        $mailer->SMTPOptions   = $mail_smtp_options;
-        $mailer->Timeout       = $mail_smtp_timeout;
-
-        return $mailer;
+        
+        if (self::$mailer == null) {
+            self::$mailer = new PHPMailer;
+        }
+        
+        self::$mailer->Priority      = $mail_priority;
+        self::$mailer->CharSet       = $mail_charset;
+        self::$mailer->ContentType   = $mail_contenttype;
+        self::$mailer->WordWrap      = $mail_wordwrap;
+        self::$mailer->Sendmail      = $mail_sendmailpath;
+        self::$mailer->Mailer        = $mail_protocol;
+        self::$mailer->SMTPDebug     = $mail_smtp_debug;
+        self::$mailer->Debugoutput   = $mail_debug_format;
+        self::$mailer->Host          = $mail_smtp_host;
+        self::$mailer->Port          = $mail_smtp_port;
+        self::$mailer->SMTPSecure    = $mail_smtp_secure;
+        self::$mailer->SMTPAutoTLS   = $mail_smtp_autotls;
+        self::$mailer->SMTPAuth      = $mail_smtp_auth;
+        self::$mailer->Username      = $mail_smtp_user;
+        self::$mailer->Password      = $mail_smtp_pass;
+        self::$mailer->SMTPKeepAlive = $mail_smtp_keepalive;
+        self::$mailer->SMTPOptions   = $mail_smtp_options;
+        self::$mailer->Timeout       = $mail_smtp_timeout;
     }
 
     /* @function boolean send_mail_gloabl(PHPMailer $mailer, string $mail, string $mail_from, string $subject, string $body, array $data)
@@ -61,16 +80,16 @@ final class Mail {
      * @return result
      */
     static function send_mail_global($mail, $mail_from, $mail_from_name, $subject, $body, $data) {
-        global $mailer;
-        if ( ! isset($mailer) )
-        {
-            \Ltb\Mail::init_mailer();
+        if (self::$mailer == null) {
+            self::init_mailer_legacy();   # for compatibility
         }
-        return \Ltb\Mail::send_mail($mailer, $mail, $mail_from, $mail_from_name, $subject, $body, $data);
+        $mailer = self::$mailer;
+        return self::send_mail($mailer, $mail, $mail_from, $mail_from_name, $subject, $body, $data);
     }
 
     /* @function boolean send_mail(PHPMailer $mailer, string $mail, string $mail_from, string $subject, string $body, array $data)
      * Send a mail, replace strings in body
+     * Should not be used directly, use send_mail_global
      * @param mailer PHPMailer object
      * @param mail Destination or array of destinations.
      * @param mail_from Sender
@@ -126,6 +145,36 @@ final class Mail {
 
         return $result;
 
+    }
+    
+    # To provide compatibility
+    static function __callStatic($name, $arguments) {
+        $count = count($arguments);
+        if ($name == "init_mailer" && $count == 0) {
+            if ($count == 0) {
+                return self::init_mailer_legacy();
+            } else {
+                return self::init_mailer_locally(extract($arguments));
+            }
+        }
+    }
+    
+    
+    # rely on global mail configured with $mail_XXXX variables
+
+    # init $mailer from config $mail_XXXX variables
+    # legacy code to be compliant with self-service-password existing configuration
+    
+    # better use init_mailer_locally directly!
+    static function init_mailer_legacy() {
+        global $mailer;
+        global $mail_priority,  $mail_charset, $mail_contenttype,  $mail_wordwrap, $mail_sendmailpath;
+        global $mail_protocol,  $mail_smtp_debug, $mail_debug_format, $mail_smtp_host, $mail_smtp_port;
+        global $mail_smtp_secure, $mail_smtp_autotls, $mail_smtp_auth, $mail_smtp_user, $mail_smtp_pass;
+        global $mail_smtp_keepalive, $mail_smtp_options, $mail_smtp_timeout;
+        
+        self::init_mailer_locally($mail_priority, $mail_charset, $mail_contenttype, $mail_wordwrap, $mail_sendmailpath, $mail_protocol, $mail_smtp_debug, $mail_debug_format, $mail_smtp_host, $mail_smtp_port, $mail_smtp_secure, $mail_smtp_autotls, $mail_smtp_auth, $mail_smtp_user, $mail_smtp_pass, $mail_smtp_keepalive, $mail_smtp_options, $mail_smtp_timeout);
+        return self::$mailer;
     }
 
 }
