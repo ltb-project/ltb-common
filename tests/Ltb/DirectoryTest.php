4837d1282b2de9c998a4e040108f10ef0a726c7c
Clément OUDOT <clement@oodo.net>
2024-08-29 17:58:11 +0200

    Tests on getUnlockDate

--- tests/Ltb/DirectoryTest.php
+++ tests/Ltb/DirectoryTest.php
@@ -155,6 +155,51 @@ final class DirectoryTest extends \Mockery\Adapter\Phpunit\MockeryTestCase
         $this->assertEquals($dt->format("Y/m/d - h:i:s"), $getLockDate->format("Y/m/d - h:i:s"), "Lock date is correct");
     }
 
+    public function test_openldap_getunlockdate_noduration(): void
+    {
+        $dt = new DateTime;
+        $phpLDAPMock = Mockery::mock('overload:Ltb\PhpLDAP');
+        $phpLDAPMock->shouldreceive([
+            'ldap_read' => null,
+            'ldap_errno' => 0,
+            'ldap_get_entries' => [
+                'count' => 1,
+                0 => [
+                    'pwdaccountlockedtime' => [
+                        'count' => 1,
+                        0 => $dt->format("Ymdhis\Z"),
+                    ]
+                ]
+            ]
+        ]);
+
+        $unlockDate = (new Ltb\Directory\OpenLDAP)->getUnlockDate(null, null, array('lockout_duration' => 0));
+        $this->assertNull($unlockDate, "Unkock date should be null");
+    }
+
+    public function test_openldap_getunlockdate_duration(): void
+    {
+        $dt = new DateTime;
+        $phpLDAPMock = Mockery::mock('overload:Ltb\PhpLDAP');
+        $phpLDAPMock->shouldreceive([
+            'ldap_read' => null,
+            'ldap_errno' => 0,
+            'ldap_get_entries' => [
+                'count' => 1,
+                0 => [
+                    'pwdaccountlockedtime' => [
+                        'count' => 1,
+                        0 => $dt->format("Ymdhis\Z"),
+                    ]
+                ]
+            ]
+        ]);
+
+        $unlockDate = (new Ltb\Directory\OpenLDAP)->getUnlockDate(null, null, array('lockout_duration' => 86400));
+        $this->assertInstanceOf("DateTime", $unlockDate, "Unlock date should be a PHP DateTime object");
+        $this->assertEquals($dt->modify("+1 day")->format("Y/m/d - h:i:s"), $unlockDate->format("Y/m/d - h:i:s"), "Unlock date is correct");
+    }
+
     public function test_activedirectory_islocked_locked_forever(): void
     {
         $ad_date = ((int)time() + 11644473600) * 10000000;
@@ -285,6 +330,52 @@ final class DirectoryTest extends \Mockery\Adapter\Phpunit\MockeryTestCase
         $getLockDate = (new Ltb\Directory\ActiveDirectory)->getLockDate(null, null);
         $this->assertInstanceOf("DateTime", $getLockDate, "Lock date should be a PHP DateTime object");
         $this->assertEquals($dt->format("Y/m/d - h:i:s"), $getLockDate->format("Y/m/d - h:i:s"), "Lock date is correct");
+    }
+
+    public function test_activedirectory_getunlockdate_noduration(): void
+    {
+        $ad_date = ((int)time() + 11644473600) * 10000000;
+        $phpLDAPMock = Mockery::mock('overload:Ltb\PhpLDAP');
+        $phpLDAPMock->shouldreceive([
+            'ldap_read' => null,
+            'ldap_errno' => 0,
+            'ldap_get_entries' => [
+                'count' => 1,
+                0 => [
+                    'lockouttime' => [
+                        'count' => 1,
+                        0 => $ad_date,
+                    ]
+                ]
+            ]
+        ]);
+
+        $unlockDate = (new Ltb\Directory\ActiveDirectory)->getUnlockDate(null, null, array('lockout_duration' => 0));
+        $this->assertNull($unlockDate, "Unock date should be null");
+    }
+
+    public function test_activedirectory_getunlockdate_duration(): void
+    {
+        $dt = new DateTime;
+        $ad_date = ((int)$dt->getTimestamp() + 11644473600) * 10000000;
+        $phpLDAPMock = Mockery::mock('overload:Ltb\PhpLDAP');
+        $phpLDAPMock->shouldreceive([
+            'ldap_read' => null,
+            'ldap_errno' => 0,
+            'ldap_get_entries' => [
+                'count' => 1,
+                0 => [
+                    'lockouttime' => [
+                        'count' => 1,
+                        0 => $ad_date,
+                    ]
+                ]
+            ]
+        ]);
+
+        $unlockDate = (new Ltb\Directory\ActiveDirectory)->getUnlockDate(null, null, array('lockout_duration' => 86400));
+        $this->assertInstanceOf("DateTime", $unlockDate, "Unlock date should be a PHP DateTime object");
+        $this->assertEquals($dt->modify("+1 day")->format("Y/m/d - h:i:s"), $unlockDate->format("Y/m/d - h:i:s"), "Unlock date is correct");
     }
 
 }
